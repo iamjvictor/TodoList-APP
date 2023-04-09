a7214f8f665c96b318941bd0709f6527c9896fb0
João Victor Asevedo <joao-victor_07@outlook.com>
2023-04-09 12:13:06 -0300

    read and show database objects

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Text, View,  Button, TextInput, StyleSheet } from 'react-native'
 import { FIRESTORE_DB } from '../config/firebaseconfig';
-import { addDoc, collection } from 'firebase/firestore';
+import { addDoc, collection, onSnapshot } from 'firebase/firestore';
 
 const List = ({ navigation}: any) =>{
 
@@ -9,7 +9,26 @@ const [tasks, setTasks] = useState<any[]>([]);
 const [task, setTask] = useState('');
 
 useEffect (() => {
-  
+  const taskRef = collection(FIRESTORE_DB, 'Task');
+
+  let subscriber = onSnapshot(taskRef, {
+    next: (snapshot) => {
+      const tasks: any[] = [];
+      snapshot.docs.forEach((doc) => {
+        
+        tasks.push({
+          id: doc.id,
+          ...doc.data()
+        })
+
+      });
+
+      setTasks(tasks)
+    },
+
+  });
+
+  return () => subscriber();
 }, []);
 
 const addTodo = async () => {
@@ -24,6 +43,16 @@ setTask('');
             <TextInput style={styles.input} placeholder='Add a new Task' onChangeText={(text: string) => setTask(text)} value={task}/>
             <Button onPress={() => addTodo()} title='ADD' disabled={task === ''}/>
         </View>
+
+        <View>
+          {tasks.map((task) => (
+            <Text key={task.id}>{task.title}</Text>
+          ))}
+        </View>
+
+
+
+
         <Button onPress={() => navigation.navigate('Details')} title='Details'/>
         
       </View>
